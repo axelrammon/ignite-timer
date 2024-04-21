0db91838251809156efbd36a67ce7234576541cc
axelrammon <axelrammon48@gmail.com>
2024-04-20 21:27:45 -0300

    adicionando repositorio ao github

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,34 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 48px;
+
+  border-radius: 4px;
+  border: 0;
+  margin: 3px;
+
+  background-color: ${props => props.theme.secondary};
+
+  /* ${props => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }} */
+
+
+
+`
